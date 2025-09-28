d2e9527b330e9107c40190713888e301748a1ca8
Ezenwankwo Emmanuel Somtoo <Somtooezenwankwo@gmail.com>
2025-09-28 23:19:16 +0100

    add a basic list interface

---
+++ Stack/MyList.java
@@ -0,0 +1,48 @@
+public class MyList {
+    private int[] listElements;
+    private int currentSize;
+    private static final int DEFAULT_CAPACITY = 10;
+
+    public MyList() {
+        listElements = new int[DEFAULT_CAPACITY];
+        currentSize = 0;
+    }
+
+    public void add(int newElement) {
+        ensureCapacity();
+        listElements[currentSize] = newElement;
+        currentSize++;
+    }
+
+    public int get(int targetIndex) {
+        validateIndex(targetIndex);
+        return listElements[targetIndex];
+    }
+
+    public void remove(int targetIndex) {
+        validateIndex(targetIndex);
+        for (int position = targetIndex; position < currentSize - 1; position++) {
+            listElements[position] = listElements[position + 1];
+        }
+        currentSize--;
+    }
+
+    public int size() {
+        return currentSize;
+    }
+
+    private void ensureCapacity() {
+        if (currentSize == listElements.length) {
+            int newCapacity = listElements.length * 2;
+            int[] expandedList = new int[newCapacity];
+            System.arraycopy(listElements, 0, expandedList, 0, listElements.length);
+            listElements = expandedList;
+        }
+    }
+
+    private void validateIndex(int targetIndex) {
+        if (targetIndex < 0 || targetIndex >= currentSize) {
+            throw new IndexOutOfBoundsException("Invalid index: " + targetIndex);
+        }
+    }
+}
